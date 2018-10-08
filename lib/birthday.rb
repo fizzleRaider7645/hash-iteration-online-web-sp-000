b087ac501418d1d47455629273b0a73f379a1252
Douglas Uretsky <douglasuretsky@gmail.com>
2018-10-08 14:21:56 +0000

    automatically backed up by learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -6,7 +6,7 @@
 
 def happy_birthday(birthday_kids)
   birthday_kids.each do |key, value|
-    put "Happy Birthday #{key}! You are now #{value} years old!"
+    puts "Happy Birthday #{key}! You are now #{value} years old!"
   end
 end
 
